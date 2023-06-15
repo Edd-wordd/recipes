9e355dc5266c410870d23278739281d1a7cbcdb4
eddwordd <edward.plasencio@gmail.com>
2023-06-15 17:21:46 -0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = 'e3a781de-0048-473b-90cc-0ce3168a2388';
+export const MODAL_CLOSE_SEC = 2.5;
